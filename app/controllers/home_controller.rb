b87adfba489e80f80326dc75edbb70c5bc6140f4
Sarah Corey <syncorey@gmail.com>
2016-01-14 02:27:25 -0500

    See issues with login and sign-up forms

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 	def index
-		@user=User.new
+		@user = User.new
 		# As soon as a user engages with the site and starts requesting pages, 
 		# an instance of that user is activated. See UserController
 	end
